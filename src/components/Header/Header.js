95c5ff05a4517688c0aa8334cd86ec8660bd0403
Rdeleon <rdeleonjr1007@yahoo.com>
2020-08-31 11:04:28 -0700

    BackgroundColors

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import sun from './img/Sun.png';
 
 function Header (){
     return(
         <div className="header jumbotron">
             <h1 className="header"><span className="header">Dulaycious</span></h1>
+            <img src={sun} alt='sun' />
             <hr />
         </div>
     )
