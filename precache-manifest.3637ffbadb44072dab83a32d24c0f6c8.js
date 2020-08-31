a3cbb80cb56fc6f2c76478ec2d495411e54bd681
Rdeleon <rdeleonjr1007@yahoo.com>
2020-08-31 07:26:51 -0700

    Updates

---
+++ precache-manifest.3637ffbadb44072dab83a32d24c0f6c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7fd4ed9eef96a0addbef7ae7e2093d",
+    "url": "/DulayciousReact/index.html"
+  },
+  {
+    "revision": "c4d2e1afb708b8f1a506",
+    "url": "/DulayciousReact/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1e0099b9c264680db414",
+    "url": "/DulayciousReact/static/css/main.0150835e.chunk.css"
+  },
+  {
+    "revision": "c4d2e1afb708b8f1a506",
+    "url": "/DulayciousReact/static/js/2.1e86fdbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DulayciousReact/static/js/2.1e86fdbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0099b9c264680db414",
+    "url": "/DulayciousReact/static/js/main.c719ea8a.chunk.js"
+  },
+  {
+    "revision": "ea31eb414ded0d60de88",
+    "url": "/DulayciousReact/static/js/runtime-main.d81c4410.js"
+  }
+]);
