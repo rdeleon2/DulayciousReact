e1601da079a62af6d55d718e11465fa4d9770a9b
Rdeleon <rdeleonjr1007@yahoo.com>
2021-03-21 20:06:13 -0700

    Prices

--- src/shared/food.js
+++ src/shared/food.js
@@ -5,9 +5,9 @@ const FOODS = [
         img: "images/dishes/FriedRice.JPG",
         description: "Stir fried rice with egg, peas, carrots. Your choice of chicken, garlic or shrimp",
         type: "Your choice of Chicken, Garlic or Shrimp",
-        price: "Chicken: Half-Pan: $45 Full-Pan: $70",
-        secondaryPrice: "Garlic: Half-Pan: $40 Full-Pan: $75 ",
-        thirdPrice: "Shrimp: Half-Pan: $40 Full-Pan: $75 "
+        price: "Chicken: Half-Pan: $40 Full-Pan: $65",
+        secondaryPrice: "Garlic: Half-Pan: $35 Full-Pan: $60",
+        thirdPrice: "Shrimp: Half-Pan: $50 Full-Pan: $70"
     },
     {
         id: 2,
@@ -15,9 +15,9 @@ const FOODS = [
         img: "images/dishes/Pancit1.JPG",
         description: "Cabbage, carrots, string beans, sugar peas, vermicelli noodles",
         type: "Your choice of Chicken, Veggie or Shrimp",
-        price: "Chicken: Half-Pan: $40 Full-Pan: $75 ",
-        secondaryPrice: "Veggie: Half-Pan: $40 Full-Pan: $75 ",
-        thirdPrice: "Shrimp: Half-Pan: $40 Full-Pan: $75 "
+        price: "Chicken: Half-Pan: $60 Full-Pan: $100 ",
+        secondaryPrice: "Veggie: Half-Pan: $40 Full-Pan: $70 ",
+        thirdPrice: "Shrimp: Half-Pan: $70 Full-Pan: $110 "
     },    
     {
         id: 3,
@@ -26,8 +26,8 @@ const FOODS = [
         description: "Choice of: Shrimp, Chicken or Beef, Cabbage, carrots, string beans, sugar peas, vermicelli noodles",
         type: "Your choice of Chicken, Beef or Shrimp", 
         price: "Chicken: Half-Pan: $40 Full-Pan: $70 ",
-        secondaryPrice: "Beef: Half-Pan: $40 Full-Pan: $75 ",
-        thirdPrice: "Shrimp: Half-Pan: $40 Full-Pan: $75 "
+        secondaryPrice: "Beef: Half-Pan: $45 Full-Pan: $75 ",
+        thirdPrice: "Shrimp: Half-Pan: $50 Full-Pan: $80 "
     },
     {
         id: 4,
@@ -36,9 +36,9 @@ const FOODS = [
         description: "Fried with Cabbage, Carrots. Sugar Peas, String Beans, Beans Sprouts, Lumpia wrapper",
         type: "Your choice of Chicken, Beef or Pork",
         sauce: "Dip Choice: Sweet Chili Sause, Garlic Vinegar (Spicy)",
-        price: "Chicken: 50pcs: $65 100pcs: $120",
+        price: "Chicken: 50pcs: $65 100pcs: $115",
         secondaryPrice: "Beef: 50pcs: $65 100pcs: $120",
-        thirdPrice: "Pork: 50pcs: $65 100pcs: $120"
+        thirdPrice: "Pork: 50pcs: $60 100pcs: $115"
     },
     {
         id: 5,
